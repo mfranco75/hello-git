e22074ec2c55fe24c6e16ca0dcc050071b13cfb5
Mariano Franco <marianofranco1975@gmail.com>
2024-09-22 19:24:48 -0300

    Segundo commit

---
+++ GitHub2.py
@@ -0,0 +1,6 @@
+import datetime as dt
+ahora = dt.datetime.now()
+fecha_hora_formateada = ahora.strftime("%d/%m/%Y %H:%M:%S")
+print("Este es el segundo archivo creado el ", fecha_hora_formateada )
+
+
